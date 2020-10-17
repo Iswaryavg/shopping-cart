5e3bc09feecdce4d96d25e01fefb7dbad22be6b3
Iswaryavg <61296893+Iswaryavg@users.noreply.github.com>
2020-10-17 23:48:32 +0100

    Updates

---
+++ precache-manifest.1c39e38ab05d0833d645e8c2eb1a6131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a34b988a0ee4d5bb2eedaf1d36b16d6",
+    "url": "/Iswaryavg/shopping-cart/index.html"
+  },
+  {
+    "revision": "0051ff0b5021e063aa03",
+    "url": "/Iswaryavg/shopping-cart/static/css/main.3c5e45bc.chunk.css"
+  },
+  {
+    "revision": "11e630cc69b931e3ff3b",
+    "url": "/Iswaryavg/shopping-cart/static/js/2.ddd1ec1b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Iswaryavg/shopping-cart/static/js/2.ddd1ec1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0051ff0b5021e063aa03",
+    "url": "/Iswaryavg/shopping-cart/static/js/main.e8640e33.chunk.js"
+  },
+  {
+    "revision": "094521690e20b1f92e5a",
+    "url": "/Iswaryavg/shopping-cart/static/js/runtime-main.fb675092.js"
+  }
+]);
